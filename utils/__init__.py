6cf8f48304ed07064dc87a386d2f04837afeae59
Antonin Raffin <antonin.raffin@dlr.de>
2018-11-13 18:40:13 +0100

    Fix for custom policy

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,1 @@
-from .utils import make_env, ALGOS, linear_schedule, create_test_env, get_trained_models
+from .utils import make_env, ALGOS, linear_schedule, create_test_env, get_trained_models, CustomDQNPolicy
