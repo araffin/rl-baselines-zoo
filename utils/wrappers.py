a8c1a8d426ff3e5e6b378abf723a55a9c9c9ac03
Antonin Raffin <antonin.raffin@dlr.de>
2019-05-28 19:56:57 +0200

    Add TimeFeature wrapper

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -1,4 +1,6 @@
 import gym
+from gym.wrappers import TimeLimit
+import numpy as np
 
 
 class DoneOnSuccessWrapper(gym.Wrapper):
@@ -18,3 +20,33 @@ class DoneOnSuccessWrapper(gym.Wrapper):
     def compute_reward(self, achieved_goal, desired_goal, info):
         reward = self.env.compute_reward(achieved_goal, desired_goal, info)
         return reward + self.reward_offset
+
+
+class TimeFeatureWrapper(gym.Wrapper):
+    """Add time to observation space for fixed length episodes."""
+
+    def __init__(self, env, max_steps=1000):
+        assert isinstance(env.observation_space, gym.spaces.Box)
+        low, high = env.observation_space.low, env.observation_space.high
+        low, high= np.concatenate((low, [0])), np.concatenate((high, [1.]))
+        env.observation_space = gym.spaces.Box(low=low, high=high, dtype=np.float32)
+
+        super(TimeFeatureWrapper, self).__init__(env)
+
+        if isinstance(env, TimeLimit):
+            self._max_steps = env._max_episode_steps
+        else:
+            self._max_steps = max_steps
+        self._current_step = 0
+
+    def reset(self):
+        self._current_step = 0
+        return self._get_obs(self.env.reset())
+
+    def step(self, action):
+        self._current_step += 1
+        obs, reward, done, info = self.env.step(action)
+        return self._get_obs(obs), reward, done, info
+
+    def _get_obs(self, obs):
+        return np.concatenate((obs, [self._current_step / self._max_steps]))
