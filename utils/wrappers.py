b698c8d71e3190b2f3a9bab08f2dbc8a46918723
Antonin Raffin <antonin.raffin@dlr.de>
2019-05-30 20:40:51 +0200

    Add test mode for time wrapper

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -30,8 +30,11 @@ class TimeFeatureWrapper(gym.Wrapper):
     :param env: (gym.Env)
     :param max_steps: (int) Max number of steps of an episode
         if it is not wrapped in a TimeLimit object.
+    :param test_mode: (bool) In test mode, the time feature is constant,
+        equal to zero. This allow to check that the agent did not overfit this feature,
+        learning a deterministic pre-defined sequence of actions.
     """
-    def __init__(self, env, max_steps=1000):
+    def __init__(self, env, max_steps=1000, test_mode=False):
         assert isinstance(env.observation_space, gym.spaces.Box)
         # Add a time feature to the observation
         low, high = env.observation_space.low, env.observation_space.high
@@ -45,6 +48,7 @@ class TimeFeatureWrapper(gym.Wrapper):
         else:
             self._max_steps = max_steps
         self._current_step = 0
+        self._test_mode = test_mode
 
     def reset(self):
         self._current_step = 0
@@ -62,4 +66,8 @@ class TimeFeatureWrapper(gym.Wrapper):
         :param obs: (np.ndarray)
         :return: (np.ndarray)
         """
-        return np.concatenate((obs, [self._current_step / self._max_steps]))
+        time_feature = self._current_step / self._max_steps
+        if self._test_mode:
+            time_feature = 0.0
+        # Optionnaly: concatenate [time_feature, time_feature ** 2]
+        return np.concatenate((obs, [time_feature]))
